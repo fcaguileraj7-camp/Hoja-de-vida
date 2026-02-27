61edd99c934113cc11ffa1bea6590ec9f099206b
fcaguileraj7-camp <fcaguileraj7@gmail.com>
2026-02-27 14:57:18 -0500

    prueba2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Menu:")
-print("1. Option 1")
-print("2. Option 2")
-print("3. Option 3")
+print("1. Opción 1")
+print("2. Opción 2")
+print("3. Opción 3")
 print("4. salir")    
-
+opcion = input("Seleccione una opción: ")   
